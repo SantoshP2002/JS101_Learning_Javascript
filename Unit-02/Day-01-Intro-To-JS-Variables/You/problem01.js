073b444c216ee7b26028700f49c0e72e14dd24b2
SantoshP2002 <santoshpawar96k@gmail.com>
2024-04-22 01:32:12 +0530

    Introduction To JS Variables All Problems Added

---
+++ Unit-02/Day-01-Intro-To-JS-Variables/You/problem01.js
@@ -0,0 +1,16 @@
+// variables
+
+var x = 10;
+console.log(x);
+
+let a; //  --->decleared the variable
+a = 20;  // --> Assigned the value
+
+console.log(a);
+ 
+
+let e = 10;
+f = 20;
+g = 30;
+
+console.log(e,f,g);
